e14f6831dc81fca238b9f6c5b8d7bb00d94d88cd
RANJAN KUMAR SAH <60313003+AakashRanjansh@users.noreply.github.com>
2022-12-23 23:20:05 +0530

    Full Functinality

---
+++ addmoney.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+    <title>Add Money</title>
+</head>
+<body style="background-color:powderblue;">
+<?php
+$voucher = $_POST["voucher"];
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "aamir";
+$user = "Aakash";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$check = "SELECT code from voucher where code = '$voucher';";
+$result = mysqli_query($conn, $check);
+
+if (mysqli_num_rows($result) > 0) {
+    $sql = "UPDATE balance set rembalance = rembalance + (select amount from voucher where code = '$voucher') where user = '$user' and (select code from voucher where code = '$voucher') = '$voucher'";
+
+    mysqli_query($conn, $sql) or die("failed to update");
+    $fetch_amount = "SELECT amount from voucher where code = '$voucher';";
+    $fetched_amount = mysqli_query($conn, $fetch_amount);
+
+    while ($row = mysqli_fetch_assoc($fetched_amount)) {
+        echo "<br><br><center><h2 style='color: green;'>" . $row["amount"] . " Rupees Added to Ur Wallet</h2></center>";
+    }
+
+    echo "<center><h4 style='color:rgb(46, 123, 165) ;'>Thank you for Recharging Wallet<h5 style='color: green;'>Please Navigate to <a href='wallet.php'>Homepage</a> to See Latest Balance...</h5></h4></center>";
+} else {
+    echo "<center><h2 style='color: red;'>Voucher Code Wrong<h3 style='color: green;'>Please Enter Correct Code...</h3> </h2></center>";
+}
+
+?>
+
+</body>
+</html>
--- wallet.php
+++ wallet.php
@@ -1,36 +1,38 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Duplicate Wallet</title>
-  <style>
+    <title>Duplicate Wallet</title>
+    <style>
 
-    .button {
-  padding: 15px 25px;
-  font-size: 24px;
-  text-align: center;
-  cursor: pointer;
-  outline: none;
-  color: #fff;
-  background-color: #04AA6D;
-  border: none;
-  border-radius: 15px;
-  box-shadow: 0 9px #999;
-}
+        .button {
+            padding: 15px 25px;
+            font-size: 24px;
+            text-align: center;
+            cursor: pointer;
+            outline: none;
+            color: #fff;
+            background-color: #04AA6D;
+            border: none;
+            border-radius: 15px;
+            box-shadow: 0 9px #999;
+        }
 
-.button:hover {background-color: #3e8e41}
+        .button:hover {
+            background-color: #3e8e41
+        }
 
-.button:active {
-  background-color: #3e8e41;
-  box-shadow: 0 5px #666;
-  transform: translateY(4px);
+        .button:active {
+            background-color: #3e8e41;
+            box-shadow: 0 5px #666;
+            transform: translateY(4px);
 
 
-}
+        }
 
     </style>
 </head>
 <body style="background-color:powderblue;">
-    <center><h1 style="color: blueviolet;">Duplicate Wallet</h1></center>
+<div style="text-align: center;"><h1 style="color: blueviolet;">Duplicate Wallet</h1></div>
 
 
 <?php
@@ -42,43 +44,42 @@ $dbname = "aamir";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
 if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: " . mysqli_connect_error());
 }
 
 $sql = "SELECT user, rembalance FROM balance";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
-  while($row = mysqli_fetch_assoc($result)) {
-    echo "<center><h2 style='color: green;'>Welcome back, " . $row["user"] . "</h2></center>";
-    echo "<br><br>";
-    echo "<center><h2>" , "  Available Balance: " . $row["rembalance"]. "</h2></center><br>";
-  }
-} 
-else 
-{
-  echo "0 result";
+    while ($row = mysqli_fetch_assoc($result)) {
+        echo "<div style=\"text-align: center;\"><h2 style='color: green;'>Welcome back, " . $row["user"] . "</h2></div>";
+        echo "<br><br>";
+        echo "<div style=\"text-align: center;\"><h2>", "  Available Balance: " . $row["rembalance"] . "</h2></center><br>";
+    }
+} else {
+    echo "0 result";
 }
 
 mysqli_close($conn);
 ?>
 <center>
-<form>
-            <table>
-                <tr>
-            <th>Enter voucher code:</th>
-            <td><input type="text" name="bal" id="bal"></td>
-        </tr>
-        <tr>
-            <td>&emsp;</td>
-        </tr>
-        <tr><th colspan="2">
-            <input type="submit" class="button" name="Add" value="Add Money">
-        </th>
-        </tr>
+    <form action="addmoney.php" method="post">
+        <table>
+            <tr>
+                <th>Enter voucher code:</th>
+                <td><input type="text" name="voucher" id="vou"></td>
+            </tr>
+            <tr>
+                <td>&emsp;</td>
+            </tr>
+            <tr>
+                <th colspan="2">
+                    <input type="submit" class="button" name="Add" value="Add Money">
+                </th>
+            </tr>
         </table>
-            
-        </form>
+
+    </form>
 </center>
 
 </body>
